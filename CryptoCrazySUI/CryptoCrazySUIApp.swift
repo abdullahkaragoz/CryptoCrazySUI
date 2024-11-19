68ca7c6a55ccb1602fe0d4a18c2ba584a277c770
abdullahkaragoz <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>
2024-11-20 00:05:48 +0300

    Initial Commit

---
+++ CryptoCrazySUI/CryptoCrazySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySUIApp.swift
+//  CryptoCrazySUI
+//
+//  Created by Abdullah Karagöz on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ CryptoCrazySUI/Service/Webservice.swift
@@ -0,0 +1,54 @@
+//
+//  Webservice.swift
+//  CryptoCrazySUI
+//
+//  Created by Abdullah Karagöz on 19.11.2024.
+//
+
+import Foundation
+
+
+class Webservice {
+    
+    func getData(from url: URL, completion: @escaping (Result<[CryptoCurrency], DownloadError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                completion(.failure(.invalidURL))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.emptyResponse))
+                return
+            }
+            
+            do {
+                let currencies = try JSONDecoder().decode([CryptoCurrency].self, from: data)
+                completion(.success(currencies))
+            } catch {
+                print("Decoding Error: \(error.localizedDescription)")
+                completion(.failure(.decodingFailed))
+            }
+            
+        }.resume()
+    }
+    
+    enum DownloadError: Error {
+        case invalidURL        // Geçersiz URL
+        case emptyResponse     // Sunucudan dönen veri yok
+        case decodingFailed    // Veri ayrıştırılamadı
+        
+        var localizedDescription: String {
+            switch self {
+            case .invalidURL:
+                return "The provided URL is invalid."
+            case .emptyResponse:
+                return "No data was received from the server."
+            case .decodingFailed:
+                return "The data could not be decoded successfully."
+            }
+        }
+    }
+}
